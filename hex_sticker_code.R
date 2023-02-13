2aeb23ecf0e2d881d1e26f14e526561b4db237bf
= <=>
2023-02-13 16:38:29 +0000

    make some changes suggested by Stephen

--- hex_sticker_code.R
+++ hex_sticker_code.R
@@ -23,20 +23,21 @@ d_summary <- group_by(d, species) %>%
 # make box plot
 p1 <- ggplot(d, aes(species, body_mass_g)) +
   geom_boxplot(aes(fill = species, col = species), outlier.shape = NA, show.legend = FALSE) +
-  stat_summary(geom = "crossbar", fatten = 0, color = "white", width = 0.4,
-    fun.data = function(x) {
-      return(c(
-        y = stats::median(x),
-        ymin = stats::median(x),
-        ymax = stats::median(x)
-      ))
-    }
-  ) +
+  #stat_summary(geom = "crossbar", fatten = 0, color = "white", width = 0.4,
+    #fun.data = function(x) {
+      #return(c(
+        #y = stats::median(x),
+        #ymin = stats::median(x),
+        #ymax = stats::median(x)
+      #))
+    #}
+  #) +
   #geom_point(position = position_jitter(width = 0.1), fill = 'white', shape = 21, size = 0.3, stroke = 0.2) +
   scale_fill_manual(values = unname(cols)) +
   scale_color_manual(values = unname(cols)) +
+  geom_line(aes(species, mean, group = group), d_summary, col = 'white', linewidth = 1.4) +
   geom_line(aes(species, mean, group = group), d_summary, linewidth = 0.8) +
-  geom_point(aes(species, mean), d_summary, size = 3, shape = 21, fill = 'white') +
+  geom_point(aes(species, mean), d_summary, size = 2, shape = 21, fill = 'white') +
   theme_void()
 
 p1
@@ -46,7 +47,7 @@ showtext_auto()
 sysfonts::font_families()
 
 # add font
-font_add_google("Bebas Neue")
+font_add_google("Outfit")
 
 # check fonts again
 sysfonts::font_families()
@@ -56,21 +57,21 @@ sticker(p1,
         package="Exeter Data",
         url = 'Analytics Hub',
         p_color = 'black',
-        p_family = "Bebas Neue",
+        p_family = "Outfit",
         p_size = 5,
-        p_x = 1,
-        p_y = 1.6,
-        u_color = 'white',
-        u_family = "Bebas Neue",
+        p_x = 1.03,
+        p_y = 1.53,
+        u_color = 'light grey',
+        u_family = "Outfit",
         u_size = 5,
-        u_x = 0.52,
-        u_y = 1.42,
+        u_x = 0.42,
+        u_y = 1.35,
         u_angle = 0,
         s_x = 1,
         s_y = 0.8,
         s_width = 1.3,
-        s_height = 1.1,
-        h_fill = 'light grey',
+        s_height = 1,
+        h_fill = 'white',
         h_color = uoe_colours('Exeter Highlight Green'),
         h_size = 5,
         filename="hex_sticker.png",
